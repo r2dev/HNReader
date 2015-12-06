175861b0b612fb05ba5bd1b3a8b7ac7e8dc53ebf
R2 <rren2@myseneca.ca>
2015-12-06 12:50:20 -0500

    Add intent for story

--- app/src/main/java/com/r2/hnreader/StoryAdapter.java
+++ app/src/main/java/com/r2/hnreader/StoryAdapter.java
@@ -1,6 +1,8 @@
 package com.r2.hnreader;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,15 +30,26 @@ public class StoryAdapter extends ArrayAdapter<Item>{
             v = vi.inflate(R.layout.story_row, null);
         }
 
-        Item p = getItem(position);
+        final Item p = getItem(position);
         if (p != null) {
             TextView title = (TextView)v.findViewById(R.id.storyTitle);
             TextView url = (TextView)v.findViewById(R.id.storyUrl);
+            TextView author = (TextView)v.findViewById(R.id.author);
             if (title != null) {
                 title.setText(p.getTitle());
             }
             if (url != null) {
-                url.setText(p.getUrl());
+                url.setText(Uri.parse(p.getUrl()).getHost());
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(p.getUrl()));
+                        getContext().startActivity(intent);
+                    }
+                });
+            }
+            if (author != null) {
+                author.setText(p.getBy());
             }
         }
         return v;
